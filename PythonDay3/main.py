0f8cbaca2921b5da0c9b2cecb7860c814e975c9f
Andrey VS <me@fragzilla.ru>
2022-05-06 10:54:26 +0300

    Урок по For in

---
+++ PythonDay3/main.py
@@ -0,0 +1,15 @@
+for number in range(3):
+    print(f"Привет мир {number}")
+
+for letter in "python":
+    print(letter.upper())
+
+exampl_string = "Я учу язык python"
+
+for word in exampl_string.split():
+    print(f'Длинна слова "{word}": {len(word)}')
+
+students_score = [1, 21, 19, 6, 5]
+
+for score in students_score:
+    print(score)
